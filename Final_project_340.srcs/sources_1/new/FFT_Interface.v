694fe395444976a377228215f8a94cafd027cd97
meeyre <eyr21002@byui.edu>
2026-07-06 11:13:15 -0600

    Added IOC FIle

--- Final_project_340.srcs/sources_1/new/FFT_Interface.v
+++ Final_project_340.srcs/sources_1/new/FFT_Interface.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/04/2026 01:00:54 PM
-// Design Name: 
-// Module Name: FFT_Interface
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module FFT_Interface(
     input wire clk, // 100MHz clk (system clk)
--- Final_project_340.srcs/sources_1/new/FFT_OUT_FIFO.v
+++ Final_project_340.srcs/sources_1/new/FFT_OUT_FIFO.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/04/2026 03:57:11 PM
-// Design Name: 
-// Module Name: FFT_OUT_FIFO
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module FFT_OUT_FIFO(
